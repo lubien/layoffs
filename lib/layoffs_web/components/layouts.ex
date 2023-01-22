7247f7ae4d4c10f354e5b8a28f3f33b09b5291b1
lubien <lubien1996@gmail.com>
2023-01-22 14:06:19 -0300

    add vsn to image

--- lib/layoffs_web/components/layouts.ex
+++ lib/layoffs_web/components/layouts.ex
@@ -2,4 +2,13 @@ defmodule LayoffsWeb.Layouts do
   use LayoffsWeb, :html
 
   embed_templates "layouts/*"
+
+  def get_image_path([last_layoff | _rest]) do
+    vsn = last_layoff.inserted_at |> to_string() |> URI.encode()
+    "/image?vsn=#{vsn}"
+  end
+
+  def get_image_path(_last_layoffs) do
+    "/image"
+  end
 end

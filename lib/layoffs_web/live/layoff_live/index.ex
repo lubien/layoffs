36bd77fcfd23e6ebc884ed0ddfd95356cd0b90ad
lubien <lubien1996@gmail.com>
2023-01-22 14:46:50 -0300

    Fix colors

--- lib/layoffs_web/live/layoff_live/index.ex
+++ lib/layoffs_web/live/layoff_live/index.ex
@@ -8,11 +8,17 @@ defmodule LayoffsWeb.LayoffLive.Index do
     {last_layoffs, callout} = Cases.get_last_layoff_with_callout()
     streaks = Cases.get_streaks()
 
+    highest_count =
+      Enum.reduce(streaks, 0, fn {_date, count}, acc ->
+        max(count, acc)
+      end)
+
     {:ok,
      assign(socket,
        page_title: callout,
        callout: callout,
        last_layoffs: last_layoffs,
+       highest_count: highest_count,
        streaks: streaks
      )}
   end
